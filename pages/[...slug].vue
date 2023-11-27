94b2a75c1c11dc2cf68eb7ef86701ab06679bf50
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2023-11-27 12:38:19 +0200

    broken links

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -14,17 +14,21 @@
 const { data: services } = await useAsyncData('services', () => queryContent('/our-services').find());
 console.log(services.value);
 
+const servicesData = []
 for (let i = 0; i < services.value.length; i++) {
-  await useAsyncData(`services+ ${i}`, () => queryContent(services.value[i]._path).find());
+  const data = await useAsyncData(`services ${i}`, () => queryContent(services.value[i]._path).find());
+  servicesData.push(data);
 }
 
 const tools = await useAsyncData('tools', () => queryContent('/products').findOne());
 const podcast = await useAsyncData('podcast', () => queryContent('/podcast').findOne());
 const aboutUS = await useAsyncData('aboutUs', () => queryContent('/about-us').findOne());
 
+const blogsData = []
 const { data: blogs } = await useAsyncData('blog', () => queryContent('/blog').find());
 for (let i = 0; i < blogs.value.length; i++) {
-  await useAsyncData(`services+ ${i}`, () => queryContent(blogs.value[i]._path).find());
+  const data = await useAsyncData(`blog ${i}`, () => queryContent(blogs.value[i]._path).find());
+  blogsData.push(data);
 }
 
 const contacts = await useAsyncData('contacts', () => queryContent('/contact').findOne());
