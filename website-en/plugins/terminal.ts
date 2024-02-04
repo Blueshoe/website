05234055dbc7d08089efd3a4594d40f86d1b8830
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2024-02-04 12:04:31 +0200

    monorepo rollback

--- website-en/plugins/terminal.ts
+++
@@ -1,5 +0,0 @@
-import VueTermynalPlugin from '@lehoczky/vue-termynal';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueTermynalPlugin);
-});
--- website-en/types/heading.ts
+++
@@ -1,6 +0,0 @@
-export type Heading = {
-  id: string;
-  title: string;
-  visible: boolean;
-  active: boolean;
-};
