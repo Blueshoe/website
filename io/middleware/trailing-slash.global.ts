f2237f0b87a5723b8cd6099d3782695f13681e1d
Michael Schilonka <michael@blueshoe.io>
2024-05-22 09:55:44 +0200

    feat: Add redirects for /hannes and /michael to Google Calendar
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

--- io/middleware/trailing-slash.global.ts
+++ io/middleware/trailing-slash.global.ts
@@ -5,5 +5,11 @@ export default function ({ path, query, hash }) {
   const nextRoute = { path: nextPath, query, hash };
 
   // 308 Permanent Redirect
+  if (path === '/hannes' || path === '/hannes/') {
+    return navigateTo("https://calendar.app.google/1c1opTAqnXqyHrsR6/", { redirectCode: 308, external: true });
+  }
+  if (path === '/michael' || path === '/michael/') {
+    return navigateTo("https://calendar.app.google/jcg6DMuBZV47i4n9A/", { redirectCode: 308, external: true });
+  }
   return navigateTo(nextRoute, { redirectCode: 308 });
 }
