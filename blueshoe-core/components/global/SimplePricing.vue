6dde9132cfaa5680055459fccf2d65e76bd7d3d2
liquidiert <haberederkorbinian@googlemail.com>
2024-06-04 16:39:08 +0200

    feat: added hasAccent for simple pricing

--- blueshoe-core/components/global/SimplePricing.vue
+++ blueshoe-core/components/global/SimplePricing.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-col md:flex-row items-center md:items-start justify-center gap-12 overflow-x-auto md:h-[800px] pt-12">
+    <div
+        :class="`flex flex-col md:flex-row items-center md:items-start justify-center gap-12 overflow-x-auto ${hasAccent ? 'md:h-[800px]' : 'md:h-[600px]'} pt-12`">
         <div v-for="(i) in cardCount" :key="i"
             class="flex flex-col items-center justify-start border-solid border-black">
             <slot :name="`card` + i" />
@@ -10,9 +11,11 @@
 <script setup lang="ts">
 interface Props {
     cardCount: number,
+    hasAccent?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    cardCount: 3
+    cardCount: 3,
+    hasAccent: true
 });
 </script>
