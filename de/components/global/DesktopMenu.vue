388bade776788368a14681fc20de7c5a07a76b8f
Lukas Rüsche <lukas@blueshoe.de>
2024-06-24 15:06:39 +0200

    fix: add close mechanism on menu

--- de/components/global/DesktopMenu.vue
+++ de/components/global/DesktopMenu.vue
@@ -1,10 +1,11 @@
 <template>
-  <ul class="flex border-b border-bs-menu-hover z-20">
+  <ul class="flex border-b border-bs-menu-hover z-20" @mouseleave="onClickOutside">
     <li
       v-for="(nav, i) in menu"
       :key="i"
       class="cursor-pointer hover:bg-bs-menu-hover px-2 xl:px-4 pt-4 pb-5 first:pr-1"
       @mouseover="nav.dropDown ? (nav.isDropDown = true) : null"
+      @mouseleave="nav.dropDown ? (nav.isDropDown = false) : null"
     >
       <NuxtLink :to="nav.href" :active-class="!nav.dropDown ? 'border-b-2 border-black border-opacity-20' : ''">
         {{ t(nav.name) }}
