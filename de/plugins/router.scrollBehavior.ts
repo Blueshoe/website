a93f4fc5eff14046d5a9719959dd723d7727b1a6
Michael Schilonka <michael@blueshoe.io>
2024-07-01 17:36:05 +0200

    fix: scroll function
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

--- de/plugins/router.scrollBehavior.ts
+++ de/plugins/router.scrollBehavior.ts
@@ -1,34 +1,27 @@
 import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.$router.options.scrollBehavior = async (to, from, savedPosition) => {
+  nuxtApp.$router.options.scrollBehavior = (to, from, savedPosition) => {
     if (savedPosition) {
       return savedPosition;
     }
 
-    const findEl = (hash, x = 0) => {
-      return (
-        document.querySelector(hash) ||
-        new Promise((resolve) => {
-          if (x > 0) {
-            return resolve(document.querySelector('#app'));
-          }
-          setTimeout(() => {
-            resolve(findEl(hash, 1));
-          }, 300);
-        })
-      );
-    };
-
-    if (to.hash) {
-      const el = await findEl(to.hash);
-
-      if ('scrollBehavior' in document.documentElement.style) {
-        return window.scrollTo({ top: el.offsetTop - 200, behavior: 'smooth' });
-      } else {
-        return window.scrollTo(0, el.offsetTop - 200);
-      }
+    if (to.hash && document.querySelector(to.hash) && document.querySelector(to.hash).getBoundingClientRect()) {
+      const nav = document.querySelector('nav');
+      console.log("Trolololo");
+      console.log(nav?.getBoundingClientRect()?.height || 0);
+      const pos =
+        window.scrollY +
+        document.querySelector(to.hash).getBoundingClientRect().top -
+        (nav?.getBoundingClientRect()?.height || 0);
+      
+      console.log(pos);
+      return {
+        top: pos,
+        behavior: 'smooth'
+      };
     }
+    console.log("Fuu");
     return { left: 0, top: 0, behaviour: 'smooth' };
   };
 });
