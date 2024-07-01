75cb3b6d1c62e227278b12a2091115a728f3061a
Michael Schilonka <michael@blueshoe.io>
2024-07-01 17:37:23 +0200

    chore: remove console logs
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

--- de/plugins/router.scrollBehavior.ts
+++ de/plugins/router.scrollBehavior.ts
@@ -8,20 +8,15 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     if (to.hash && document.querySelector(to.hash) && document.querySelector(to.hash).getBoundingClientRect()) {
       const nav = document.querySelector('nav');
-      console.log("Trolololo");
-      console.log(nav?.getBoundingClientRect()?.height || 0);
       const pos =
         window.scrollY +
         document.querySelector(to.hash).getBoundingClientRect().top -
         (nav?.getBoundingClientRect()?.height || 0);
-      
-      console.log(pos);
       return {
         top: pos,
         behavior: 'smooth'
       };
     }
-    console.log("Fuu");
     return { left: 0, top: 0, behaviour: 'smooth' };
   };
 });
